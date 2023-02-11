5298952f2ebad235a49816c1b08655fb19931714
Jonathan Gregg <gregg.jonathan@gmail.com>
2023-02-10 16:30:16 -0800

    First pass of Members SQL complete

--- DML.sql
+++ DML.sql
@@ -1,5 +1,6 @@
 -- get all members and their information for the browse Member page --
-SELECT Members.member_id, Members.f_name, Members.l_name, Members.address, Members.birthday, Members.membership_plan_id, Members.gym_id
+SELECT Members.member_id, Members.f_name, Members.l_name, Members.address,
+    Members.birthday, Members.membership_plan_id, Members.gym_id
 FROM Members 
 INNER JOIN Membership_plans ON Members.membership_plan_id = Membership_plans.membership_plan_id 
 
@@ -11,5 +12,17 @@ VALUES (:f_nameInput, :l_nameInput, :addressInput, :birthdayInput, :membership_p
 SELECT Members.member_id, Members.f_name, Members.l_name, Members.address, Members.birthday, Members.membership_plan_id, Members.gym_id
 FROM Members WHERE member_id = :member_id_selected_from_browse_member_page
 
+UPDATE Members
+SET f_name = :new_first_name,
+    l_name = :new_last_name,
+    address = :new_address,
+    birthday = :new_birthday,
+    membership_plan_id = :new_membership_plan_id,
+    gym_id = :new_gym
+WHERE id = :id_from_form
+
 -- Delete member
+DELETE FROM Members
+WHERE member_id = :id_from_form
+
 
