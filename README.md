c737172e47c69e3310d75f657b7b426b5c5b4b86
Edward Mai <maie@flip2.engr.oregonstate.edu>
2023-02-24 14:07:45 -0800

    initial project template

--- README.md
+++
@@ -1,2 +0,0 @@
-# CS340-Project
-Database Project for CS340. Group 36
--- app.py
+++ app.py
@@ -1,20 +1,42 @@
-from flask import Flask, render_template, json, redirect
-from flask_mysqldb import MySQL
-from flask import request
+from flask import Flask, render_template, json
+import database.db_connector as db
 import os
 
+# Configuration
+
 app = Flask(__name__)
-mysql = MySQL(app)
 
+db_connection = db.connect_to_database()
 
-# Routes
+# Routes 
+
 @app.route('/')
-def index():
-    return render_template('index.html')
+def root():
+    return render_template('index.j2')
+
+@app.route('/members')
+def members():
+    query = "SELECT * from Members;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template('members.j2', members=results)
+
+@app.route('/gyms')
+def gyms():
+    query = "SELECT * from Gyms;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template('gyms.j2', gyms=results)
+
+
+
 
 
 # Listener
-if __name__ == "__main__":
 
-    #Start the app on port 3000, it will be different once hosted
-    app.run(port=2489, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 9111)) 
+    #                                 ^^^^
+    #              You can replace this number with any valid port
+    
+    app.run(port=port, debug=True)
