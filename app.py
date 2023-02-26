30742cf28853b24ccf9920b35946c31401a1c5f2
Jonathan Gregg <gregg.jonathan@gmail.com>
2023-02-26 11:23:43 -0800

    Start "plans" work (multiple files affected)

--- app.py
+++ app.py
@@ -156,12 +156,122 @@ def edit_member(Member_ID):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+############################################################################
+
 @app.route("/plans", methods=["GET", "POST"])
 def plans():
-    pass
-@app.route("/gyms", methods=["POST", "GET"])
-def gyms():
-    pass
+    if request.method == "GET":
+        query = "SELECT Plans.plan_id AS 'Membership Plan ID', \
+                Plans.monthly_fee AS 'Monthly Fee', \
+                Plans.weight_cardio AS 'Weights/Cardio', \
+                Plans.spa_room AS 'Spa Room', \
+                Plans.lap_pool AS 'Lap Pool', \
+                Plans.ballcourt AS 'Ballcourt' \
+                FROM Plans"
+        cursor = mysql.connection.cursor()
+        cursor.execute(query)
+        data = cursor.fetchall()
+        return render_template("plans.j2", data = data)
+
+    if request.method == "POST":
+
+        #activates when adding a new plan.
+        if request.form.get("addPlan"):
+            # grab user form inputs
+            monthly_fee = request.form["monthly_fee"]
+            weight_cardio = request.form["weight_cardio"]
+            spa_room = request.form["spa_room"]
+            lap_pool = request.form["lap_pool"]
+            ballcourt = request.form["ballcourt"]
+
+            query = "INSERT INTO Plans(monthly_fee, weight_cardio, spa_room, lap_pool, ballcourt) \
+                    VALUES (%s, %s, %s, %s, %s)"
+            cursor = mysql.connection.cursor()
+            cursor.execute(query,(monthly_fee, weight_cardio, spa_room, lap_pool, ballcourt))
+            mysql.connection.commit()
+            return redirect("/plans")
+
+
+# handle deletion of members
+@app.route("/delete_plan/<int:plan_id>")
+def delete_plan(plan_id):
+    #mySQL query to delete the member with our passed id
+    query = "DELETE FROM Plans WHERE plan_id = %s;"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (plan_id,))
+    mysql.connection.commit()
+    return redirect('/plans')
+
+
+#########################################################################
+# handle editing plans
+@app.route("/edit_plan/<int:plan_id>", methods=["GET", "POST"])
+def edit_plan(plan_id):
+    if request.method == "GET":
+        # mySQL query to grab the infor of the person with our passed id
+        query = "SELECT  Plans.monthly_fee, \
+                Plans.weight_cardio, \
+                Plans.spa_room, \
+                Plans.lap_pool, \
+                Plans.ballcourt \
+                FROM Plans WHERE plan_id = %s"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        return render_template('edit_plan.j2', data=data)
+    
+    if request.method == "POST":
+        # fire off if user clicks the "Edit Person" button
+        if request.form.get("Edit_Member"):
+            # grab user form inputs
+            f_name = request.form["f_name"]
+            l_name = request.form["l_name"]
+            address = request.form["address"]
+            birthday = request.form["birthday"]
+            plan_id = request.form["plan_id"]
+            gym_id = request.form["gym_id"]
+
+            # account for cases where gym_id is set to null
+            if gym_id == "0":
+                query = "INSERT INTO Members (f_name, l_name, address, birthday, plan_id) VALUES (%s, %s, %s, %s, %s)"
+                cur = mysql.connection.cursor()
+                cur.execute(query,(f_name, l_name, address, birthday, plan_id))
+                mysql.connection.commit()
+
+            else:
+                query = "INSERT INTO Members (f_name, l_name, address, birthday, plan_id, gym_id) VALUES (%s, %s, %s, %s, %s, %s)"
+                cur = mysql.connection.cursor()
+                cur.execute(query,(f_name, l_name, address, birthday, plan_id, gym_id))
+                mysql.connection.commit()
+
+            return redirect("/members")
+#########################################################################
+
+
+
+
+
+
+
+# @app.route("/gyms", methods=["GET", "POST"])
+# def gyms():
+#     if request.method == "GET":
+#         pass
+#     if request.method == "POST":
+#         pass
+#     pass
 
 # @app.route("/instructors")
 # @app.route("/classes")
