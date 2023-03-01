f9c62206c811c893c61737b52ae1401026bf5f66
Jonathan Gregg <gregg.jonathan@gmail.com>
2023-02-28 17:17:39 -0800

    Add delete route

--- app.py
+++ app.py
@@ -429,7 +429,14 @@ def class_participants():
 
             return redirect("/class_participants")
 
-
+# handle deletion of class_participants
+@app.route("/delete_class/<int:member_class_ID>")
+def delete_class(member_class_ID):
+    query = "DELETE FROM Class_participants WHERE member_class_ID = %s;"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (member_class_ID,))
+    mysql.connection.commit()
+    return redirect('/classes')
 
 
 
